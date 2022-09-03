d3c1eb53f7a6e20f38499e86b94dd7f0d57d7700
SpeziMM <magnus@muellerjena.de>
2022-09-03 17:23:08 +0200

    Initial Commit

---
+++ GymApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GymApp
+//
+//  Created by Magnus Müller on 03.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GymApp/GymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppApp.swift
+//  GymApp
+//
+//  Created by Magnus Müller on 03.09.22.
+//
+
+import SwiftUI
+
+@main
+struct GymAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
